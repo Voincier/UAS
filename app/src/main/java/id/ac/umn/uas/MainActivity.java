695cfb534d0c4d875312027f20c5f46d6cedb13c
joyamadea <amadeajoy@gmail.com>
2020-06-04 04:46:31 +0700

    Login and Register

--- app/src/main/java/id/ac/umn/uas/MainActivity.java
+++ app/src/main/java/id/ac/umn/uas/MainActivity.java
@@ -1,14 +1,78 @@
 package id.ac.umn.uas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    ProgressDialog loading;
+    EditText eEmail, ePassword;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth=FirebaseAuth.getInstance();
+        loading = new ProgressDialog(this);
+
+        eEmail = findViewById(R.id.email_log);
+        ePassword = findViewById(R.id.pw_log);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = eEmail.getText().toString();
+                String pw = ePassword.getText().toString();
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)){
+                    Toast.makeText(MainActivity.this, "All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    loading.setMessage("Logging In");
+                    login(email,pw);
+                }
+            }
+        });
+    }
+
+    protected void login(String email,String password){
+        auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            loading.dismiss();
+                            Intent i = new Intent(MainActivity.this,ListActivity.class);
+                            startActivity(i);
+                            finish();
+
+                        }
+                        else{
+                            loading.hide();
+                            Toast.makeText(MainActivity.this,"Incorrect email or password",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/id/ac/umn/uas/RegisterActivity.java
+++ app/src/main/java/id/ac/umn/uas/RegisterActivity.java
@@ -1,14 +1,108 @@
 package id.ac.umn.uas;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button btnRegis;
+    EditText eName, eEmail, ePassword, eConfirm;
+    ProgressDialog loading;
+    DatabaseReference ref;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        auth=FirebaseAuth.getInstance();
+
+        eName = findViewById(R.id.name_reg);
+        eEmail = findViewById(R.id.email_reg);
+        ePassword = findViewById(R.id.pw_reg);
+        eConfirm = findViewById(R.id.pw2_reg);
+
+        btnRegis = findViewById(R.id.regisBtn);
+        loading = new ProgressDialog(this);
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = eName.getText().toString();
+                String email = eEmail.getText().toString();
+                String pw = ePassword.getText().toString();
+                String confirm = eConfirm.getText().toString();
+
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(pw)){
+                    Toast.makeText(RegisterActivity.this, "All fields are required",Toast.LENGTH_SHORT).show();
+                }
+                else if(!pw.equals(confirm)){
+                    Toast.makeText(RegisterActivity.this,"Password does not", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    loading.setMessage("Create your profile");
+                    loading.show();
+                    register(name, email, pw);
+                }
+            }
+        });
     }
+
+    private void register(final String name, final String email, final String password){
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser fbUser=auth.getCurrentUser();
+                            String userID=fbUser.getUid();
+                            ref= FirebaseDatabase.getInstance().getReference("user").child(userID);
+
+                            HashMap<String, String> hashMap=new HashMap<>();
+                            hashMap.put("id",userID);
+                            hashMap.put("name",name);
+                            hashMap.put("email",email);
+                            hashMap.put("password",password);
+
+                            ref.setValue(hashMap, new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                                    loading.dismiss();
+                                    Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                                }
+                            });
+
+                        }
+                        else{
+                            loading.hide();
+                            Toast.makeText(RegisterActivity.this,"Register Failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+
 }
