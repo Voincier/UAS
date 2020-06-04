11a22d238b63a711c06ae2c40f8b56b625f29996
joyamadea <amadeajoy@gmail.com>
2020-06-04 10:11:43 +0700

    Dark mode + about app

--- app/src/main/java/id/ac/umn/uas/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/id/ac/umn/uas/ui/dashboard/DashboardFragment.java
@@ -1,21 +1,29 @@
 package id.ac.umn.uas.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import org.w3c.dom.Text;
+
+import id.ac.umn.uas.AboutMeActivity;
 import id.ac.umn.uas.R;
 
 public class DashboardFragment extends Fragment {
-
+    Switch sb;
+    TextView aboutAppBtn;
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,7 +31,30 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        sb = root.findViewById(R.id.darkMode);
 
+        aboutAppBtn = root.findViewById(R.id.aboutApp);
+
+        aboutAppBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), AboutMeActivity.class);
+                startActivity(i);
+            }
+        });
+        sb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    //tDark.setText("Dark Mode On");
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else{
+                    //tDark.setText("Dark Mode Off");
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
         return root;
     }
 }
